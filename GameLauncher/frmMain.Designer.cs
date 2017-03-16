4efaf59cfd287fba3fb8ef602dce131c141357c6
Dominic Houghton <technpizza@gmail.com>
2017-03-16 21:42:01 +0000

    Initial code commit

---
+++ GameLauncher/frmMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace GameLauncher
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.cmdLaunch = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picImage
+            // 
+            this.picImage.Location = new System.Drawing.Point(13, 13);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(700, 200);
+            this.picImage.TabIndex = 0;
+            this.picImage.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 247);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(603, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // cmdLaunch
+            // 
+            this.cmdLaunch.Location = new System.Drawing.Point(621, 247);
+            this.cmdLaunch.Name = "cmdLaunch";
+            this.cmdLaunch.Size = new System.Drawing.Size(90, 23);
+            this.cmdLaunch.TabIndex = 2;
+            this.cmdLaunch.Text = "Launch";
+            this.cmdLaunch.UseVisualStyleBackColor = true;
+            this.cmdLaunch.Click += new System.EventHandler(this.cmdLaunch_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatus.Location = new System.Drawing.Point(12, 231);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 281);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.cmdLaunch);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.picImage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Game Launcher";
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button cmdLaunch;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ GameLauncher/frmMain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace GameLauncher
+{
+    public partial class frmMain : Form
+    {
+        iniParser options;
+        public frmMain()
+        {
+            InitializeComponent();
+            options = new iniParser("launcher.ini");
+            Image i = Image.FromFile("logo.png");
+            picImage.Image = i;
+        }
+
+        private String getResponse(string url)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            request.UserAgent = "GameLauncher";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            return new StreamReader(response.GetResponseStream()).ReadToEnd();
+        }
+
+        private void cmdLaunch_Click(object sender, EventArgs e)
+        {
+            cmdLaunch.Enabled = false;
+            cmdLaunch.Text = "Launching...";
+            lblStatus.Text = "Checking for latest version...";
+            Application.DoEvents();
+            String releasejson = getResponse("https://api.github.com/repos/" + options.getValue("Launcher", "repo") + "/releases");
+            dynamic release = JToken.Parse(releasejson);
+            String asseturl = release[0].assets[0].url.ToString();
+
+            String assetjson = getResponse(asseturl);
+            dynamic asset = JToken.Parse(assetjson);
+            String downloadurl = asset.browser_download_url.ToString();
+            DateTime updatetime = DateTime.Parse(asset.updated_at.ToString());
+
+            DateTime currenttime = DateTime.Parse(options.getValue("Game", "update"));
+
+            if (DateTime.Compare(currenttime, updatetime) < 0)
+            {
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += Client_DownloadProgressChanged;
+                client.DownloadFileCompleted += Client_DownloadFileCompleted;
+                client.Headers.Add("User-Agent", "GameLauncher");
+                client.DownloadFileAsync(new Uri(downloadurl), "game.exe");
+
+                options.setValue("Game", "update", asset.updated_at.ToString());
+            } else
+            {
+                Process.Start("game.exe");
+                Application.Exit();
+            }
+        }
+
+        private void Client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            Process.Start("game.exe");
+            options.Save("launcher.ini");
+            Application.Exit();
+            //throw new NotImplementedException();
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = e.BytesReceived;
+            double totalBytes = e.TotalBytesToReceive;
+            double percentage = bytesIn / totalBytes * 100;
+            progressBar1.Value = (int)percentage;
+
+            lblStatus.Text = String.Format("{0}/{1} bytes downloaded...", bytesIn, totalBytes);
+        }
+    }
+}
